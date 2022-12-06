e1f238d2410aa979c81191bce834429bed95bb25
carraes <carraeshb@gmail.com>
2022-12-06 11:00:51 -0300

    chore: d6

--- D6/d6.js
+++ D6/d6.js
@@ -1,5 +1,36 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('./data.txt', 'utf8').split('\n');
+const data = fs.readFileSync('./data.txt', 'utf8').split('\n').join('');
 
-console.log(data);
+const isUnique = (str) => {
+  return new Set(str).size === str.length;
+};
+
+const checkChar = (str) => {
+  const arrStr = str.split('');
+  const result = '';
+  for (let i = 0; i < arrStr.length - 3; i++) {
+    let a = '';
+    const count = i + 4;
+    for (let j = i; j < count; j++) {
+      a += arrStr[j];
+    }
+    if (isUnique(a)) return i + 4;
+  }
+  return result;
+};
+
+const checkCharTwo = (str) => {
+  const arrStr = str.split('');
+  for (let i = 0; i < arrStr.length - 13; i++) {
+    let a = '';
+    const count = i + 14;
+    for (let j = i; j < count; j++) {
+      a += arrStr[j];
+    }
+    if (isUnique(a)) return i + 14;
+  }
+};
+
+console.log(checkChar(data));
+console.log(checkCharTwo(data));
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 # Advent of Code [2022]
 
-- [x] Day 1
-- [x] Day 2
-- [x] Day 3
-- [x] Day 4
-- [x] Day 5
-- [ ] Day 6
+- [x] Day 1 ⭐⭐
+- [x] Day 2 ⭐⭐
+- [x] Day 3 ⭐⭐
+- [x] Day 4 ⭐⭐
+- [x] Day 5 ⭐⭐
+- [x] Day 6 ⭐⭐
 - [ ] Day 7
 - [ ] Day 8
 - [ ] Day 9
